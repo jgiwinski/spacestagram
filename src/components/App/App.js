e6b635407f01f14488bd64f36c82d109536e1e10
jgiwinski <juliaiwinski@gmail.com>
2021-09-19 19:58:28 -0700

    Like and unlike func done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,39 +8,56 @@ class App extends Component {
     super(); 
     this.state = {
       posts: [],
-      favorites: this.getFromLocal() || [],
+      liked: [],
       error: '',
     }
   }
+
+  // this.getFromLocal() ||
 
   componentDidMount() {
     getData()
       .then(response => this.setState({posts: response}))
       .catch(error => this.setState({error: error}))
   }
-
+  
   displayContent = () => {
-    return this.state.posts.map(post => {
+    return this.state.posts.map((post, i) => {
       return <Post 
-        id={post.date}
+        key={i}
+        id={post.title}
         date={post.date}
         image={post.url}
         title={post.title}
         desc={post.explanation} 
+        likeUnlikePhoto={this.likeUnlikePhoto}
       />
     })
   }
-
+  
   // ---- saving favorites to local ----
   saveToLocal = () => {
-    const toStore = JSON.stringify(this.state.favorites)
-    localStorage.setItem('savedPhotos', toStore)
+    const toStore = JSON.stringify(this.state.liked)
+    localStorage.setItem('likedPhotos', toStore)
   }
-
+  
   getFromLocal = () => {
-    const getStore = localStorage.getItem('savedPhotos')
+    const getStore = localStorage.getItem('likedPhotos')
     const parsedStore = JSON.parse(getStore); 
     return parsedStore; 
+  }
+  
+  likeUnlikePhoto = async (e) => {
+    e.preventDefault() 
+    if(!this.state.liked.includes(e.target.id)){
+       await this.setState({ liked: [...this.state.liked, e.target.id] });
+       this.saveToLocal();
+    } else {
+      const updatedLiked = this.state.liked.filter(post => {
+        return post !== e.target.id
+      })
+      this.setState({ liked: updatedLiked })
+    }
   }
 
   render() {
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,14 +1,15 @@
 import './Post.scss'; 
 import React from 'react';
 
-export const Post = ({ id, title, date, image, desc }) => {
+export const Post = ({ id, title, date, image, desc, likeUnlikePhoto}) => {
     return (
         <article>
-            <img id={id} src={image} alt='apod'/>
+            <img src={image} alt={title}/>
             <h2>{title}</h2>
             <h3>{date}</h3>
             <p>{desc}</p>
-            <button>LIKE</button>
+            <h2>{id}</h2>
+            <button id={id} onClick={e => likeUnlikePhoto(e)}>LIKE</button>
         </article>
     )
 }
