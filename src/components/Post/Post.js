30d74992ea2512b20c7b889a42bde687d6a4c7f0
jgiwinski <juliaiwinski@gmail.com>
2021-09-20 15:42:55 -0700

    Format earth date

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,13 +2,22 @@ import './Post.scss';
 import React from 'react';
 
 export const Post = ({ id, title, date, image, desc, likeUnlikePhoto}) => {
+
+    const slashDate = date.replaceAll('-', '/')
+    const formatDate = inputDate => {
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let date = new Date(inputDate);
+        return monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear() ;
+    }
+
     return (
         <article>
-            <img src={image} alt={title}/>
-            <h2>{title}</h2>
-            <h3>{date}</h3>
+            <img className='apod' src={image} alt={title}/>
+            <div className='title-date'>
+                <h2>{title}</h2>
+                <h3>{formatDate(slashDate)}</h3>
+            </div>
             <p>{desc}</p>
-            <h2>{id}</h2>
             <button id={id} onClick={e => likeUnlikePhoto(e)}>LIKE</button>
         </article>
     )
